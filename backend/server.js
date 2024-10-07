102b1103af6f2c113d5ffb83ea418525838963cc
MaxLuz <91216647+MaxLuz@users.noreply.github.com>
2024-10-07 19:26:48 -0400

    added mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const songRoutes = require("./routes/songs");
 
 // express app
@@ -17,7 +18,15 @@ app.use((req, res, next) => {
 // routes
 app.use("/api/songs", songRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("listening on port 4000!!");
-});
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests only when connected to database
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db, and listening on port 4000!!");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
