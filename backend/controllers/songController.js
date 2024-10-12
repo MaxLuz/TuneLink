4c21cbb3aabdffd891a609f2069f8a7236b961ec
MaxLuz <91216647+MaxLuz@users.noreply.github.com>
2024-10-12 14:59:52 -0400

    finished API for song CRUD

--- backend/controllers/songController.js
+++ backend/controllers/songController.js
@@ -1,8 +1,29 @@
 const Song = require("../models/songModel.js");
+const mongoose = require("mongoose");
 
 // get all songs
+const getSongs = async (req, res) => {
+  const songs = await Song.find({}).sort({ createdAt: -1 });
+
+  res.status(200).json(songs);
+};
 
 // get a single song
+const getSong = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such song" });
+  }
+
+  const song = await Song.findbyId(id);
+
+  if (!song) {
+    return res.status(404).json({ error: "No such song" });
+  }
+
+  res.status(200).json(song);
+};
 
 // create a new song entry
 const createSong = async (req, res) => {
@@ -18,8 +39,49 @@ const createSong = async (req, res) => {
 };
 
 // delete a song
+const deleteSong = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such song" });
+  }
+
+  const song = await Song.findOneAndDelete({ _id: id });
+
+  if (!song) {
+    return res.status(404).json({ error: "No such song" });
+  }
+
+  res.status(200).json({ song });
+};
+
+// update a song
+const updateSong = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such song" });
+  }
+
+  const song = await Song.findOneAndUpdate(
+    { _id: id },
+    {
+      ...req.body,
+    }
+  );
+
+  if (!song) {
+    return res.status(404).json({ error: "No such song" });
+  }
+
+  res.status(200).json({ song });
+};
 
 // update a song
 module.exports = {
   createSong,
+  getSongs,
+  getSong,
+  deleteSong,
+  updateSong,
 };
--- backend/routes/songs.js
+++ backend/routes/songs.js
@@ -1,28 +1,26 @@
 const express = require("express");
-const { createSong } = require("../controllers/songController");
+const {
+  createSong,
+  getSongs,
+  getSong,
+  deleteSong,
+  updateSong,
+} = require("../controllers/songController");
 const router = express.Router();
 
 // GET all songs
-router.get("/", (req, res) => {
-  res.json({ mssg: "GET all songs" });
-});
+router.get("/", getSongs);
 
 // GET a single song
-router.get("/:id", (req, res) => {
-  res.json({ mssg: "GET a single song" });
-});
+router.get("/:id", getSong);
 
 // POST a new song
 router.post("/", createSong);
 
 // DELETE a new song
-router.delete("/:id", (req, res) => {
-  res.json({ mssg: "DELETE a song" });
-});
+router.delete("/:id", deleteSong);
 
 // UPDATE a new song
-router.patch("/:id", (req, res) => {
-  res.json({ mssg: "UPDATE a new song" });
-});
+router.patch("/:id", updateSong);
 
 module.exports = router;
