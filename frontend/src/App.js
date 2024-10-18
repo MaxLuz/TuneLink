7c95781dc2b71ac78f03b361218662549dba55a2
MaxLuz <91216647+MaxLuz@users.noreply.github.com>
2024-10-18 13:23:01 -0400

    added spotify data to home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,12 @@
 import "./App.css";
 import React, { useState } from "react";
-import { SpotifyAuth, SpotifyAuthListener } from "react-spotify-auth";
-import "react-spotify-auth/dist/index.css"; // Import the styles for the Spotify login button
-import TopArtists from "./components/TopArtists";
+
 import Navbar from "./components/Navbar";
-import TopSongs from "./components/TopSongs";
-import Welcome from "./components/Welcome";
-import Buttons from "./components/Buttons";
+
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [token, setToken] = useState(localStorage.getItem("spotifyAuthToken"));
-  const [spotuser, setSpotuser] = useState("not-logged-in");
-  const [timeframe, setTimeframe] = useState("short_term");
-
   return (
     <div className="App">
       <BrowserRouter>
@@ -25,34 +17,6 @@ function App() {
           </Routes>
         </div>
       </BrowserRouter>
-
-      <div className="main-content">
-        {/* Listener to automatically store the token in localStorage */}
-        <SpotifyAuthListener onAccessToken={(token) => setToken(token)} />
-
-        {!token ? (
-          // Spotify login button
-          <SpotifyAuth
-            redirectUri="http://localhost:3000/" // Redirect after Spotify authentication
-            clientID="df95797d307541d79a1e2a2d3bc3e072"
-            scopes={["user-top-read"]} // Scopes required for your app
-          />
-        ) : (
-          // data displays when user is authenticated
-          <div className="isAuthenticated">
-            <Welcome
-              token={token}
-              spotuser={spotuser}
-              setSpotuser={setSpotuser}
-            />
-            <Buttons timeframe={timeframe} setTimeframe={setTimeframe} />
-            <div className="data-components">
-              <TopArtists token={token} timeframe={timeframe} />
-              <TopSongs token={token} timeframe={timeframe} />
-            </div>
-          </div>
-        )}
-      </div>
     </div>
   );
 }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSongsContext } from "../hooks/useSongContext";
+import { SpotifyAuth, SpotifyAuthListener } from "react-spotify-auth";
+import "react-spotify-auth/dist/index.css"; // Import the styles for the Spotify login button
 // components
 import SongDetails from "../components/SongDetails";
 import SongForm from "../components/SongForm";
+import TopArtists from "../components/TopArtists";
+import TopSongs from "../components/TopSongs";
+import Welcome from "../components/Welcome";
+import Buttons from "../components/Buttons";
 
 const Home = () => {
+  const [token, setToken] = useState(localStorage.getItem("spotifyAuthToken"));
+  const [spotuser, setSpotuser] = useState("not-logged-in");
+  const [timeframe, setTimeframe] = useState("short_term");
+
   const { songs, dispatch } = useSongsContext();
 
   // fetches all of the current favorite songs
@@ -24,6 +34,36 @@ const Home = () => {
   }, []);
   return (
     <div className="home">
+      <div className="main-content">
+        {/* Listener to automatically store the token in localStorage */}
+        <SpotifyAuthListener onAccessToken={(token) => setToken(token)} />
+
+        {!token ? (
+          // Spotify login button
+          <SpotifyAuth
+            redirectUri="http://localhost:3000/" // Redirect after Spotify authentication
+            clientID="df95797d307541d79a1e2a2d3bc3e072"
+            scopes={["user-top-read"]} // Scopes required for your app
+          />
+        ) : (
+          // data displays when user is authenticated
+          <div className="isAuthenticated">
+            <Welcome
+              token={token}
+              spotuser={spotuser}
+              setSpotuser={setSpotuser}
+            />
+            <Buttons timeframe={timeframe} setTimeframe={setTimeframe} />
+            <div className="data-components-wrapper">
+              <div className="data-components">
+                <TopArtists token={token} timeframe={timeframe} />
+                <TopSongs token={token} timeframe={timeframe} />
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+
       <div className="songs">
         {songs &&
           songs.map((song) => <SongDetails key={song._id} song={song} />)}
