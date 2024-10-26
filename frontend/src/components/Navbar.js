4fa2f78279fac4be9b1067f06e99ffe88b8811ef
MaxLuz <91216647+MaxLuz@users.noreply.github.com>
2024-10-26 15:29:14 -0400

    added login functionality and navbar conditions

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,9 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   const handleClick = () => {
     logout();
@@ -16,13 +18,18 @@ const Navbar = () => {
         <h1>TuneLink</h1>
       </Link>
       <nav className="nav">
-        <div className="logout">
-          <button onClick={handleClick}>Log out</button>
-        </div>
-        <div className="login-signup">
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign up</Link>
-        </div>
+        {user && (
+          <div className="logout">
+            <span>{user.email}</span>
+            <button onClick={handleClick}>Log out</button>
+          </div>
+        )}
+        {!user && (
+          <div className="login-signup">
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign up</Link>
+          </div>
+        )}
       </nav>
     </header>
   );
