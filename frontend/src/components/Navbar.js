31a26acebcc00dd8436c9f9a60094c3dd43dff71
MaxLuz <91216647+MaxLuz@users.noreply.github.com>
2024-10-26 15:14:09 -0400

    added logout function

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,16 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
+import { useLogout } from "../hooks/useLogout";
 
 const Navbar = () => {
+  const { logout } = useLogout();
+
+  const handleClick = () => {
+    logout();
+  };
+
   return (
     <header className="nav-container">
       <Link className="logo" to="/">
         <h1>TuneLink</h1>
       </Link>
       <nav className="nav">
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Sign up</Link>
+        <div className="logout">
+          <button onClick={handleClick}>Log out</button>
+        </div>
+        <div className="login-signup">
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Sign up</Link>
+        </div>
       </nav>
     </header>
   );
