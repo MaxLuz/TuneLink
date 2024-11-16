28a802d2738b60c71c47312b9f60df1ddf179cc7
MaxLuz <91216647+MaxLuz@users.noreply.github.com>
2024-11-16 16:03:09 -0500

    changed friends list color

--- frontend/src/components/FriendDetails.js
+++ frontend/src/components/FriendDetails.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "../styles/FriendDetails.css";
 
 const FriendDetails = ({ friend }) => {
-  return <p>{friend}</p>;
+  return <p className="friend-details">{friend}</p>;
 };
 
 export default FriendDetails;
