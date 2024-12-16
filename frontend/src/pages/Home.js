6f7dcddd6e0ac80cdbb781457fd1f667d51bfb47
MaxLuz <91216647+MaxLuz@users.noreply.github.com>
2024-12-16 10:57:04 -0500

    made listening habits its own component

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -14,6 +14,7 @@ import Buttons from "../components/Buttons";
 import Hero from "../components/Hero";
 import FriendDetails from "../components/FriendDetails";
 import SideNav from "../components/SideNav";
+import ListeningHabits from "../components/ListeningHabits";
 // styles
 import "../styles/Home.css";
 import "../styles/Navbar.css";
@@ -124,39 +125,7 @@ const Home = () => {
         </div>
         <div className="dashboard-content">
           <div className="dashboard-content-right">
-            <div className="listening-habits">
-              <div className="listening-habits-top">
-                <div className="listening-habits-text">
-                  <h2 className="lh-title">Listening Habits</h2>
-                  <p className="lh-text">
-                    Your favorites from this
-                    <span className="time-period"> month</span>
-                  </p>
-                </div>
-                <button className="toggle-button">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                    <path
-                      class="fa-secondary"
-                      opacity=".4"
-                      d="M0 256C0 397.4 114.6 512 256 512s256-114.6 256-256S397.4 0 256 0L224 0l0 32 0 64 0 32 64 0 0-32 0-29.3C378.8 81.9 448 160.9 448 256c0 106-86 192-192 192S64 362 64 256c0-53.7 22-102.3 57.6-137.1L76.8 73.1C29.5 119.6 0 184.4 0 256z"
-                    />
-                    <path
-                      class="fa-primary"
-                      d="M176 142.1l17 17 80 80 17 17L256 289.9l-17-17-80-80-17-17L176 142.1z"
-                    />
-                  </svg>
-                  Toggle
-                </button>
-              </div>
-              <div className="listening-habits-bottom">
-                <div className="tracks-container">
-                  <TopSongs token={spotifytoken} timeframe={timeframe} />
-                </div>
-                <div className="artists-container">
-                  <TopArtists token={spotifytoken} timeframe={timeframe} />
-                </div>
-              </div>
-            </div>
+            <ListeningHabits timeframe={timeframe} />
             <div className="home-dash-friends">
               <div className="share-a-song"></div>
               <div className="friends-list"></div>
